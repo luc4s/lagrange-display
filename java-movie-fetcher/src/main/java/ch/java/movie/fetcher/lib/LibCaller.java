a67fc321954df3d0dd532def927b0fea5938b56d
Simon Jobin <simon.jobin@stemys.io>
2023-12-20 15:11:47 +0100

    redirect external process logs

--- java-movie-fetcher/src/main/java/ch/java/movie/fetcher/lib/LibCaller.java
+++ java-movie-fetcher/src/main/java/ch/java/movie/fetcher/lib/LibCaller.java
@@ -2,7 +2,10 @@ package ch.java.movie.fetcher.lib;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 
 @Slf4j
 @Service
@@ -10,11 +13,23 @@ public class LibCaller {
 
     public void displayBMP() {
         try {
+
             ProcessBuilder processBuilder = new ProcessBuilder("./epd");
+            processBuilder.redirectErrorStream(true);
             Process process = processBuilder.start();
 
+            InputStream inputStream = process.getInputStream();
+            BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
+
+            // Read and log the output
+            String line;
+            while ((line = reader.readLine()) != null) {
+                log.info("epd logs : {}", line);
+            }
+
+            // Wait for the process to finish
             int exitCode = process.waitFor();
-            log.info("Exit Code: " + exitCode);
+            log.info("External Process exited with code: {}", exitCode);
 
         } catch (IOException | InterruptedException e) {
             Thread.currentThread().interrupt();
