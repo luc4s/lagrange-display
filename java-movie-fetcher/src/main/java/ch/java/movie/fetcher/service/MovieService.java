6d474de7a561f6ba616dc55e0f7c550dae07667d
Simon Jobin <simon.jobin@stemys.io>
2023-12-30 15:59:06 +0100

    try chromium headless with Process builder

--- java-movie-fetcher/src/main/java/ch/java/movie/fetcher/service/MovieService.java
+++ java-movie-fetcher/src/main/java/ch/java/movie/fetcher/service/MovieService.java
@@ -18,6 +18,8 @@ import org.springframework.stereotype.Service;
 import javax.imageio.ImageIO;
 import java.awt.image.BufferedImage;
 import java.io.ByteArrayInputStream;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 
 @Slf4j
 @Service
@@ -26,7 +28,7 @@ public class MovieService {
     @Value("${URL:https://cinelagrange.ch/feed}")
     private String urlJSON;
 
-    @Value("${URL:https://192.168.1.108.ch?display=compact}")
+    @Value("${URL:https://192.168.1.108:3001?display=compact}")
     private String urlPNG;
 
     public MovieSchedule fetchMovieScheduleJSON() {
@@ -53,16 +55,40 @@ public class MovieService {
 
     public BufferedImage fetchMovieSchedulePNG() {
 
-        System.setProperty("webdriver.chrome.driver", "/usr/bin/chromedriver");
-        ChromeOptions options = new ChromeOptions();
-        WebDriver driver = new ChromeDriver(options);
-        options.addArguments("--headless", "--disable-gpu", "--window-size=984x1305", "--screenshot=output.png", "--disable-software-rasterizer");
+//        System.setProperty("webdriver.chrome.driver", "/usr/local/bin/chromedriver");
+//        ChromeOptions options = new ChromeOptions();
+//        WebDriver driver = new ChromeDriver(options);
+//        options.addArguments("--headless", "--disable-gpu", "--window-size=984x1305", "--screenshot=output.png", "--disable-software-rasterizer");
+//
+//        try {
+//            driver.get(this.urlPNG);
+//            byte[] screenshot = ((TakesScreenshot) driver).getScreenshotAs(OutputType.BYTES);
+//            return ImageIO.read(new ByteArrayInputStream(screenshot));
+//        } catch (Exception e) {
+//            log.error("Error while fetching the resource", e);
+//        }
 
         try {
-            driver.get(this.urlPNG);
-            byte[] screenshot = ((TakesScreenshot) driver).getScreenshotAs(OutputType.BYTES);
+
+            String[] command = {
+                "chromium-browser",
+                "--headless",
+                "--disable-gpu",
+                "--screenshot=output.png",
+                "--window-size=984x1305",
+                "--disable-software-rasterizer",
+                this.urlPNG
+            };
+
+            ProcessBuilder processBuilder = new ProcessBuilder(command);
+            Process process = processBuilder.start();
+            process.waitFor();
+
+            byte[] screenshot = Files.readAllBytes(Paths.get("output.png"));
             return ImageIO.read(new ByteArrayInputStream(screenshot));
+
         } catch (Exception e) {
+            Thread.currentThread().interrupt();
             log.error("Error while fetching the resource", e);
         }
 
